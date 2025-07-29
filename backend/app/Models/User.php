b7ec0613780667ed21743cf921c1766efb527947
Yoshiki Yokoyama <y.yokoyfb@gmail.com>
2025-07-29 14:36:36 +0900

    StudyLogのモデル、マイグレーション、フォームリクエストを作成

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function studyLogs()
+    {
+        return $this->hasMany(StudyLog::class);
+    }
 }
