897bd13c83a0e953b1e3887bb089da0ba068c859
Yoshiki Yokoyama <y.yokoyfb@gmail.com>
2025-07-31 10:35:29 +0900

    不要なファイルの削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { StudyLogList } from "./pages/StudyLogList";
-import { CreateStudyLog } from "./pages/CreateStudyLog";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StudyLogList />} />
-        <Route path="/new" element={<CreateStudyLog />} />
       </Routes>
     </Router>
   );
